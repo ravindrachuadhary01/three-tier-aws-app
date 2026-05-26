6a4ca01fc78cbeccc2b07d980d19fd7d758079a0
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-26 17:58:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,9 @@ data "aws_availability_zones" "azs" {}
 # -----------------------------
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 # -----------------------------
